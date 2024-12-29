4cb47fc5ec3b06dec7a06db086ce229ca1b23723
ChinhQuoc <dthiep.gm@gmail.com>
2024-12-29 16:17:39 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  // apiServerUrl: 'https://casting-agency-api-65072a915e1a.herokuapp.com/',
   apiServerUrl: 'http://127.0.0.1:5000',
   auth0: {
     url: 'dev-5l37br2fze6hxdk6.us',
